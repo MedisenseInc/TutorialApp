818368ee6aed778cc12b5863a99895c82853d684
M. Wada <wada_muramasa@nifty.com>
2026-01-12 18:03:31 +0900

    Initial Commit

---
+++ TutorialApp/TutorialAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialAppApp.swift
+//  TutorialApp
+//
+//  Created by Masayuki Wada on 2026/01/12.
+//
+
+import SwiftUI
+
+@main
+struct TutorialAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
